c9f2b0bcb5118e2c466ee1ebb109f43afc95698b
Diaxium <Diaxium.Developer.01@outlook.com>
2025-03-01 17:08:42 -0800

    Generated documentation

--- docs/core/utils/package-search-index.js
+++ docs/core/utils/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.modular_multi_loader_template"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.modular_multi_loader_template"},{"l":"com.example.modular_multi_loader_template.functions"},{"l":"com.example.modular_multi_loader_template.functions.logging"}];updateSearchResults();
