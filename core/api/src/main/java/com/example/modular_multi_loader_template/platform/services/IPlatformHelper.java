d2fdcd2dab9e23ea8214735e8e5b41689f1f962f
Diaxium <Diaxium.Developer.01@outlook.com>
2025-03-01 17:05:20 -0800

    Updated IPlatformHelper.java
    Added simple getInfo() method, for getting all platform related information and mods.

--- core/api/src/main/java/com/example/modular_multi_loader_template/platform/services/IPlatformHelper.java
+++ core/api/src/main/java/com/example/modular_multi_loader_template/platform/services/IPlatformHelper.java
@@ -1,5 +1,9 @@
 package com.example.modular_multi_loader_template.platform.services;
 
 
+import com.example.modular_multi_loader_template.platform.types.PlatformInfo;
+
 public interface IPlatformHelper {
+    PlatformInfo getInfo();
+
 }
